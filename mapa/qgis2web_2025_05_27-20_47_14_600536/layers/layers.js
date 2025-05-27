f36326643061ee899cbbe0128a041a7687d7019f
EmmaPena <emmapenacoll8@gmail.com>
2025-05-27 19:49:15 +0200

    Pagina web juli

---
+++ mapa/qgis2web_2025_05_27-20_47_14_600536/layers/layers.js
@@ -0,0 +1,16 @@
+var wms_layers = [];
+
+
+        var lyr_GoogleMaps_0 = new ol.layer.Tile({
+            'title': 'Google Maps',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: ' ',
+                url: 'https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}'
+            })
+        });
+
+lyr_GoogleMaps_0.setVisible(true);
+var layersList = [lyr_GoogleMaps_0];
